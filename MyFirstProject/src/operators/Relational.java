480bef62fd12c276263dd7ca1a1057fe15820096
Shobharani292 <shobharanimitte593@gmail.com>
2023-08-29 22:22:05 +0530

    Relational opertors example

---
+++ MyFirstProject/src/operators/Relational.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class Relational {
+    public static void main(String[] args) 
+    {
+        int a = 10, b = 1;
+        System.out.println("a is " + a + " and b is " + b);
+
+        // == operator
+        System.out.println(a == b);  // false
+
+        // != operator
+        System.out.println(a != b);  // true
+
+        // > operator
+        System.out.println(a > b);  // true
+
+        // < operator
+        System.out.println(a < b);  // false
+
+        // >= operator
+        System.out.println(a >= b);  // true
+
+        // <= operator
+        System.out.println(a <= b);  // false
+    }
+    
+}
