03b0f93e6a1fd6d9d01766f4febd8a0099b05eb8
Shobharani292 <shobharanimitte593@gmail.com>
2023-08-29 22:37:07 +0530

    getter and setter

---
+++ MyFirstProject/src/constructor/Getter_Setter.java
@@ -0,0 +1,34 @@
+package constructor;
+
+public class Getter_Setter {
+    private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public static void main(String[] args) {
+		Getter_Setter  ob = new Getter_Setter();
+		ob.setId(56);
+        ob.setName("Shobha");
+		System.out.println(ob.getId());
+        System.out.println(ob.getName());
+	    
+
+	}
+    
+}
