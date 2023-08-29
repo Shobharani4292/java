390ecd5adb23e5e5852e86e10a2b1b793b740c50
Shobharani292 <shobharanimitte593@gmail.com>
2023-08-29 22:42:35 +0530

    Employee file added

---
+++ MyFirstProject/src/ems/Employee.java
@@ -0,0 +1,36 @@
+package ems;
+
+public class Employee 
+{
+    private int eid;
+	private String ename;
+	private float esalary;
+	private String edept;
+
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getEsalary() {
+		return esalary;
+	}
+	public void setEsalary(float esalary) {
+		this.esalary = esalary;
+	}
+	public String getEdept() {
+		return edept;
+	}
+	public void setEdept(String edept) {
+		this.edept = edept;
+	}
+
+    
+}
