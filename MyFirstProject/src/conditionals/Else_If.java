9c9dbd7d2bd30e2d4cbd6d719cf9d15fed6438b1
Shobharani292 <shobharanimitte593@gmail.com>
2023-08-29 22:30:58 +0530

    Else if ladder

---
+++ MyFirstProject/src/conditionals/Else_If.java
@@ -0,0 +1,41 @@
+package conditionals;
+
+import java.util.Scanner;
+
+public class Else_If {
+    public static void main(String[] args) {
+        char ch;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character:");
+        ch = sc.next().charAt(0);
+            
+        //else if ladder to whether entered character is vowel or not
+        if(ch=='a')
+        {
+            System.out.println(ch + " is a vowel");
+        }
+        else if(ch=='e')
+        {
+            System.out.println(ch+ " is a vowel");
+        }
+        else if(ch=='i')
+        {
+            System.out.println(ch+ " is a vowel");
+        }
+        else if(ch=='o')
+        {
+            System.out.println(ch + " is a vowel");
+        }
+        else if(ch=='u')
+        {
+            System.out.println(ch +" is a vowel");
+        }
+        else 
+        {
+            System.out.println(ch+ " is not a vowel");
+        }
+        sc.close();
+    }
+
+    
+}
