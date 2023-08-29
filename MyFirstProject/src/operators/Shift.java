64e04d06dc8402150e3b4b8aae322ff3b2df2a41
Shobharani292 <shobharanimitte593@gmail.com>
2023-08-29 22:20:01 +0530

    Shift operators example

---
+++ MyFirstProject/src/operators/Shift.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Shift {
+    public static void main(String[] args) {
+		int a = 5;
+		System.out.println(a<<2);//5= 00000101 right shift => 00010100 
+		System.out.println(a>>2);//5 =00000101 left shift => 00000001
+
+	}
+
+    
+}
